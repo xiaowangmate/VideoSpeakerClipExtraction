3a9ecead42fffe412456c4e988a23886723a87a0
kingone <8969629+kingoneone@user.noreply.gitee.com>
2024-06-04 15:20:00 +0800

    update

--- face_detection/speaker_detection.py
+++ face_detection/speaker_detection.py
@@ -63,6 +63,7 @@ class SpeakerDetector:
         is_contained = False
         for (top_right_y, top_right_x, left_bottom_y, left_bottom_x) in face_locations:
             face = img[top_right_y - 50:left_bottom_y + 50, left_bottom_x - 50:top_right_x + 50]
+            face = cv2.cvtColor(face, cv2.COLOR_BGR2RGB)
             face_encoding = face_recognition.face_encodings(face)
             if face_encoding:
                 results = face_recognition.compare_faces([self.speaker_face], face_encoding[0])
